f5436b2a42466761196fc6f88e3f30dccce91713
justsilencia <silencedogood848@gmail.com>
2021-07-21 13:21:24 -0500

    Commit resource files

---
+++ src/pages/contactus.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const ContactUs = () => {
+    return (
+        <Layout>
+            <SEO />
+            <div className="mt-5">
+                Contact us            
+            </div>
+        </Layout>
+  );
+}
+
+export default ContactUs;
---
+++ src/pages/signup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+ const SignUp = () => (
+     <Layout>
+         <SEO title="Home" />
+        <div className="mt-5">
+            test
+        </div>
+     </Layout>
+);
+
+export default SignUp;
